ad57f685ef0b146acc0d5545d93efe16d5fc9439
DwArFeng <915724865@qq.com>
2026-05-20 21:40:13 +0800

    scg-api 子模块类优化注释、文档注释格式、代码换行格式
    com.dwarfeng.scg.api.configuration.GenerateConfiguration
    
    Signed-off-by: DwArFeng <915724865@qq.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,9 @@
 
 ### 功能构建
 
+- `scg-api` 子模块类优化注释、文档注释格式、代码换行格式。
+  - com.dwarfeng.scg.api.configuration.GenerateConfiguration。
+
 - `scg-impl` 子模块类优化注释、文档注释格式、代码换行格式。
   - com.dwarfeng.scg.impl.bean.BeanMapper。
   - com.dwarfeng.scg.impl.bean.entity.HibernateCommonVariable。
--- scg-api/src/test/java/com/dwarfeng/scg/api/configuration/GenerateConfiguration.java
+++ scg-api/src/test/java/com/dwarfeng/scg/api/configuration/GenerateConfiguration.java
@@ -13,7 +13,6 @@ public class GenerateConfiguration {
 
     private final GenerateService generateService;
 
-    // test.scg_setting_id
     @Value("#{new com.dwarfeng.subgrade.stack.bean.key.StringIdKey('${test.scg_setting_id}')}")
     private StringIdKey scgSettingKey;
 
